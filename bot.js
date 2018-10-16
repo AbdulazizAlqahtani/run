79105dc47a8aed17dc03b8c15fdaf26fac634f2f
Sellmox5 <42848880+RESKORESKO65@users.noreply.github.com>
2018-10-16 23:59:42 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,11 +1,35 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const prefix = '!'
 const adminprefix = "!";
 const developers = ['501486494402215947'];
-
-console.log("Paaaaartly");
-
-client.on('message', message => {
+ client.on('ready', () => {
+ console.log("hi"); 
+console.log("bye");
+});
+ client.on('message', message => {
+  if (message.content === 'ping') {
+    message.reply('Pong!');
+  }
+});
+ client.on('message', message => {
+  if (!message.content.startsWith(prefix)) return;
+  var args = message.content.split(' ').slice(1);
+  var argresult = args.join(' ');
+  if (message.author.id !== "482639461306335232") return;
+   
+  if (message.content.startsWith(prefix + 'setwatching')) {
+  client.user.setActivity(argresult, {type: 'WATCHING'})
+     console.log('watching be' + argresult);
+    message.channel.sendMessage(`Watch Now: **${argresult}**`)
+} 
+  
+  if (message.content.startsWith(prefix + 'setlis')) {
+  client.user.setActivity(argresult, {type: 'LISTENING'})
+     console.log('LISTENING be' + argresult);
+    message.channel.sendMessage(`LISTENING Now: **${argresult}**`)
+} 
+ client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!developers.includes(message.author.id)) return;
       
