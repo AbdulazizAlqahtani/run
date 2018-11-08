c12ced89c5908e58682f6d5c1a736c08ccb00411
Sellmox5 <42848880+SPLITLPTEOEORK2003@users.noreply.github.com>
2018-11-08 17:24:28 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,5 @@
 const Discord = require('discord.js');
-const client = new Discord.Client1();
-const client = new Discord.Client2();
+const client = new Discord.Client();
 console.log("Scrpit By Dream");
 
 
@@ -12,6 +11,5 @@ channel.send(`Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I
 })
  
  
-client1.login("NTA4NzI5NjgyNzQ5OTQ3OTA0.DsIE1g.niKeyC67Xh4aNSh83pgWNJkabSY");
+client.login("NTA4NzI5NjgyNzQ5OTQ3OTA0.DsIE1g.niKeyC67Xh4aNSh83pgWNJkabSY");
 
-client2.login("NTA5NDQ3OTIzMjM5NzQ3NTg2.DsOSVA.PVRv6slx-eGZXE4I0aeOX_54IL0")
