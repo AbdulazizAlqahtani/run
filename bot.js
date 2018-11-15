96e20ca57952442efe6dd2815d601c4f48312498
Sellmox5 <42848880+SPLITLPTEOEORK2003@users.noreply.github.com>
2018-11-15 20:49:17 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,14 +1,88 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
-console.log("Scrpit By Dream");
+const client2 = new Discord.Client();
+
+client.on('ready', () => {
+   console.log(`----------------`);
+   console.log(`Credit Farmm - Script By : Kahrbaa `);
+   console.log(`----------------`);
+   console.log(`Loadinng`);
+   console.log(`Loadinng.`);
+   console.log(`Loadinng..`);
+   console.log(`Loadinng...`);
+   console.log(`This Bots Online ' `);
+   console.log(`----------------`);
+});
 
 
-client.on("ready", () => {
-let channel =     client.channels.get("509046139085914123")
-setInterval(function() {
-channel.send(`Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi , I'm Sad . Hi ,`);
-}, 25)
-})
- 
- 
-client.login("NTA4NzI5NjgyNzQ5OTQ3OTA0.DsIE1g.niKeyC67Xh4aNSh83pgWNJkabSY");
+client.on('message', message => {
+    if(message.content === '-راتب'){
+        message.channel.send('#daily')
+    }
+});
+
+client.on('message', message => {
+    if(message.content === '-مبلغ'){
+        message.channel.send('#credits')
+    }
+});
+
+client.on('message', message => {
+    if(message.content === '-ريب'){
+        message.channel.send('#credits')
+    }
+});
+
+client2.on('message', message => {
+    if(message.content === '-راتب'){
+        message.channel.send('#daily')
+    }
+});
+
+client2.on('message', message => {
+    if(message.content === '-مبلغ'){
+        message.channel.send('#credits')
+    }
+});
+
+client2.on('message', message => {
+    if(message.content === '-ريب'){
+        message.channel.send('#credits')
+    }
+});
+
+
+
+client.on('message', message => { // لا تغير شئ عشان ما تخرب الدنيا
+if (message.content === '!spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 90000; x++) {
+        message.channel.send(`**Credit Spam , Frame , Credit By Kahrba , Farm Credit By Kahrba, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج **[ " ${x} " ]`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+
+client2.on('message', message => { // لا تغير شئ عشان ما تخرب الدنيا
+if (message.content === '!spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 90000; x++) {
+        message.channel.send(`**Credit Spam , Frame , Credit By Kahrba , Farm Credit By Kahrba, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج **[ " ${x} " ]`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+
+
+
+
+client.login(process.env.TOKEN);// لا تغير فيها شيء
+client2.login(process.env.TOKEN2);// لا تغير فيها شيء
